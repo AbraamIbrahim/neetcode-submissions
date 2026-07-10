1b781720d5300a2f8d9ff617c2c1ff473da4ee61
Fathom <abraamibr8@gmail.com>
2026-07-09 20:41:25 -0400

    Add: find-duplicate-integer - submission-6

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-6.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int n = nums.length;
+        for(int i = 0; i < n; i++){
+            System.out.println(Arrays.toString(nums));
+            int indexVal = nums[Math.abs(nums[i]) - 1];
+            if(indexVal < 0){
+                return Math.abs(nums[i]);
+            }
+            else{
+                nums[Math.abs(nums[i]) - 1] *= - 1;
+            }
+        }
+        for(int i = n - 1; i > 0; i--){
+            int indexVal = nums[Math.abs(nums[i]) - 1];
+            if(indexVal < 0){
+                return Math.abs(nums[i]);
+            }
+            else{
+                nums[Math.abs(nums[i]) - 1] *= - 1;
+            }
+        }
+        return 0;
+    }
+}
